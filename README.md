0cfb5e6f9c1cbb4f5ef34dbebd6789a20e5574ba
gszep <grisha.szep@gmail.com>
2025-06-26 17:17:38 +0900

    successful LLM control

---
+++ README.md
@@ -0,0 +1,7 @@
+### Quick start
+
+This project uses the uv package manager (https://github.com/astral-sh/uv)
+
+```bash
+uv pip install -e .
+```
---
+++ tests/test_genlm.py
@@ -0,0 +1,35 @@
+import asyncio
+from genlm.control import PromptedLLM, BoolFSA, AWRS
+
+
+def test_regex():
+    # Create a language model potential.
+    llm = PromptedLLM.from_name("gpt2")
+    llm.set_prompt_from_str("Here is my honest opinion:")
+
+    # Create a finite-state automaton potential using a regular expression.
+    fsa = BoolFSA.from_regex(r" SMC is (🔥🔥|😍😍|🤌🤌) with LMs")
+
+    # Coerce the FSA so that it operates on the token type of the language model.
+    coerced_fsa = fsa.coerce(llm, f=b"".join)
+
+    # Create a token sampler that combines the language model and FSA.
+    token_sampler = AWRS(llm, coerced_fsa)
+
+    # Generate text using SMC.
+    # Generation is asynchronous; use `await` if calling in an async context (like in an async
+    # function or in a Jupyter notebook) and `asyncio.run(token_sampler.smc(...))` otherwise.
+    sequences = asyncio.run(
+        token_sampler.smc(
+            n_particles=10,  # Number of candidate sequences to maintain
+            ess_threshold=0.5,  # Threshold for resampling
+            max_tokens=30,  # Maximum sequence length
+            verbosity=1,  # Print particles at each step
+        )
+    )
+
+    sequences.decoded_posterior
+    # Example output:
+    # {
+    #   ' SMC is 🔥🔥 with LMs': 1.0,
+    # }
