576d6ead3b6371c287769531fb68c683288c4ae9
gszep <grisha.szep@gmail.com>
2025-07-23 22:18:14 +0900

    remove dependency on with_cancellation

--- oath_keepers/vllm_server.py
+++ oath_keepers/vllm_server.py
@@ -1,13 +1,13 @@
 import uvicorn
-from fastapi import FastAPI, Request
-from fastapi.responses import JSONResponse, Response
+from fastapi import FastAPI
 from openai.types.chat import ChatCompletion, ChatCompletionMessage
 from openai.types.chat.chat_completion import Choice
 from openai.types.completion_usage import CompletionUsage
 from vllm import LLM
-from vllm.entrypoints.utils import with_cancellation
 from vllm.logger import init_logger
 from vllm.utils import random_uuid
+
+from oath_keepers.types import GenerateRequest
 
 logger = init_logger("vllm_server")
 
@@ -15,69 +15,53 @@ llm = LLM(model="google/gemma-3-1b-it")
 app = FastAPI()
 
 
-@app.post("/generate")
-async def generate(request: Request) -> Response:
-    """Generate completion for the request.
-
-    The request should be a JSON object with the following fields:
-    - prompt: the prompt to use for the generation.
-    - stream: whether to stream the results or not.
-    - other fields: the sampling parameters (See `SamplingParams` for details).
-    """
-    request_dict = await request.json()
-    return await _generate(request_dict, raw_request=request)
-
-
-@with_cancellation
-async def _generate(request_dict: dict, raw_request: Request) -> Response:
-    prompts = request_dict.pop("prompts")
+@app.post("/generate", response_model=ChatCompletion)
+async def generate(request: GenerateRequest) -> ChatCompletion:
     sampling_params = llm.get_default_sampling_params()
     request_id = random_uuid()
 
-    for key, value in request_dict.pop("sampling_params").items():
+    for key, value in request.sampling_params.model_dump().items():
         setattr(sampling_params, key, value)
 
     response = llm.generate(
         f"""
             <start_of_turn>user
-            {prompts}<end_of_turn>
+            {request.prompts}<end_of_turn>
             <start_of_turn>model
         """,
         sampling_params=sampling_params,
         use_tqdm=False,
     )[0]
 
-    return JSONResponse(
-        ChatCompletion(
-            choices=[
-                Choice(
-                    message=ChatCompletionMessage(
-                        content=output.text,
-                        role="assistant",
-                        tool_calls=None,
-                        function_call=None,
-                        refusal=None,
-                        annotations=None,
-                        audio=None,
-                    ),
-                    finish_reason="stop",
-                    index=output.index,
-                )
-                for output in response.outputs
-            ],
-            id=request_id,
-            model="vLLM",
-            object="chat.completion",
-            created=0,
-            usage=CompletionUsage(
-                completion_tokens=sum(len(output.token_ids) for output in response.outputs),
-                prompt_tokens=len(response.prompt_token_ids),
-                total_tokens=(
-                    sum(len(output.token_ids) for output in response.outputs)
-                    + len(response.prompt_token_ids)
+    return ChatCompletion(
+        choices=[
+            Choice(
+                message=ChatCompletionMessage(
+                    content=output.text,
+                    role="assistant",
+                    tool_calls=None,
+                    function_call=None,
+                    refusal=None,
+                    annotations=None,
+                    audio=None,
                 ),
+                finish_reason="stop",
+                index=output.index,
+            )
+            for output in response.outputs
+        ],
+        id=request_id,
+        model="vLLM",
+        object="chat.completion",
+        created=0,
+        usage=CompletionUsage(
+            completion_tokens=sum(len(output.token_ids) for output in response.outputs),
+            prompt_tokens=len(response.prompt_token_ids),
+            total_tokens=(
+                sum(len(output.token_ids) for output in response.outputs)
+                + len(response.prompt_token_ids)
             ),
-        ).model_dump()
+        ),
     )
 
 
