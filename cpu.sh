c2235ba30f54c6372b8b5ec3ee2f14ca7232f90f
gszep <grisha.szep@gmail.com>
2025-11-02 01:02:48 +0900

    revert compiler version

--- cpu.sh
+++ cpu.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 # install system dependencies
-sudo apt-get install -y --no-install-recommends gcc-12 g++-12 libnuma-dev python3-dev ffmpeg
-sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-12 10 --slave /usr/bin/g++ g++ /usr/bin/g++-12
+sudo apt-get install -y --no-install-recommends libnuma-dev ffmpeg
 
 # build vllm from source for CPU backend (https://docs.vllm.ai/en/latest/getting_started/installation/cpu.html#intelamd-x86_2)
 git clone https://github.com/vllm-project/vllm.git vllm_source
@@ -15,7 +14,7 @@ VLLM_TARGET_DEVICE=cpu python setup.py install
 cd ..
 
 # clean up
-rm -rf vllm_source
+sudo rm -rf vllm_source
 
 # install whisperlivekit
 uv pip install --no-cache-dir whisperlivekit
