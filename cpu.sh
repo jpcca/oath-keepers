de664ddf1ba29eb28e7d4ee28e87f9bbde2bfa05
gszep <grisha.szep@gmail.com>
2025-11-02 01:39:44 +0900

    use recommended gcc compiler for vllm

--- cpu.sh
+++ cpu.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # install system dependencies
-sudo apt-get install -y --no-install-recommends libnuma-dev ffmpeg
+sudo apt-get install -y --no-install-recommends gcc-12 g++-12 libnuma-dev python3-dev ffmpeg
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-12 10 --slave /usr/bin/g++ g++ /usr/bin/g++-12
 
 # build vllm from source for CPU backend (https://docs.vllm.ai/en/latest/getting_started/installation/cpu.html#intelamd-x86_2)
 git clone https://github.com/vllm-project/vllm.git vllm_source
@@ -10,7 +11,7 @@ cd vllm_source
 uv pip install -r requirements/cpu-build.txt --torch-backend cpu --index-strategy unsafe-best-match
 uv pip install -r requirements/cpu.txt --torch-backend cpu --index-strategy unsafe-best-match
 
-VLLM_TARGET_DEVICE=cpu uv pip install . --no-build-isolation
+VLLM_TARGET_DEVICE=cpu uv pip install . --no-build-isolation # build backend returned an error - can be ignored for some reason
 cd ..
 
 # clean up
