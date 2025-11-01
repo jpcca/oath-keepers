89056989b34c02c458aad91489dc52ddaaf8367e
Grisha Szep <contact@gszep.com>
2025-11-01 23:58:09 +0900

    restore --index-strategy unsafe-best-match

--- cpu.sh
+++ cpu.sh
@@ -8,14 +8,11 @@ sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-12 10 --slave /
 git clone https://github.com/vllm-project/vllm.git vllm_source
 cd vllm_source
 
-uv pip install -r requirements/cpu-build.txt --torch-backend cpu
-uv pip install -r requirements/cpu.txt --torch-backend cpu
+uv pip install -r requirements/cpu-build.txt --torch-backend cpu --index-strategy unsafe-best-match
+uv pip install -r requirements/cpu.txt --torch-backend cpu --index-strategy unsafe-best-match
 
 VLLM_TARGET_DEVICE=cpu uv pip install . --no-build-isolation
 cd ..
 
 # clean up
 sudo rm -rf vllm_source
-
-# install whisperlivekit
-uv pip install --no-cache-dir whisperlivekit
